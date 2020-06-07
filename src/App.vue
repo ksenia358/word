f2996140e13aaf3cce02451670d85944773ca6d6
ksenia <k_deriyova@detoc.ru>
2020-06-07 17:59:42 +0300

    2 добавила кнопку, добавила стили

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Button class="full" type="button" title="Я кнопка!"></Button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Button from './components/Elements/Button/Button.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Button
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import "./assets/styles/normalize.scss";
+import "./assets/styles/helpers.scss";
+// import "./assets/fonts/roboto/roboto.css";
 
 Vue.config.productionTip = false
 
