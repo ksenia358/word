3722d3aae3379ea795f83c88ad5cdc7920eeba39
ksenia <k_deriyova@detoc.ru>
2020-06-07 17:24:01 +0300

    2 компонент кнопка

---
+++ src/components/Elements/Button/Button.vue
@@ -0,0 +1,49 @@
+<template>
+  <button :class="getClass" :type="type" @click="Click">
+    <template>
+      {{title}}
+    </template>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    type: {
+      type: String,
+      required: false,
+      default: 'submit'
+    },
+    title: {
+      type: String,
+      required: false,
+      default: null
+    },
+    fullwidth: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    onClick: {
+      type: Function,
+      required: false,
+      default: () => {}
+    },
+
+  },
+  computed: {
+    getClass() {
+      return {
+        "full": this.fullwidth,
+      };
+    }
+  },
+  methods: {
+    Click(){
+      this.onClick();
+    }
+  }
+}
+</script>
+<style lang="scss" src="./Button.scss" scoped />
