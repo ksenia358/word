10c5848fa259848f82ba8c5a4b7450f5d413aaac
ksenia <k_deriyova@detoc.ru>
2020-06-07 18:19:19 +0300

    2 шрифты

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import "./assets/styles/normalize.scss";
-import "./assets/styles/helpers.scss";
-// import "./assets/fonts/roboto/roboto.css";
+import "./assets/styles/style.scss";
 
 Vue.config.productionTip = false
 
