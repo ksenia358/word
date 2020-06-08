93e749c146b44b7596d6aac167a642789e1ac31a
Denis Nikonov <nikon1624@gmail.com>
2020-06-08 23:23:58 +0500

    Добавлен тест(нужно еще пофиксить)

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,19 @@
   <div id="app" class="page">
     <Header></Header>
     <div class="main">
-
+      <Test></Test>
     </div>
   </div>
 </template>
 
 <script>
   import Header from './components/Header/Header.vue'
+  import Test from './components/Test/Test';
 
   export default {
     name: 'App',
     components: {
-      Header,
+      Header, Test
     }
   }
 </script>
