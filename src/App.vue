805900f89022306bfa59efe27af0714fa96de626
Denis Nikonov <nikon1624@gmail.com>
2020-06-08 09:51:28 +0500

    Удален HelloWorld.vue

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,18 @@
   <div id="app" class="page">
     <Header></Header>
     <div class="main">
-      <HelloWorld msg="Welcome to Your Vue.js App"/>
+
     </div>
   </div>
 </template>
 
 <script>
   import Header from './components/Header/Header.vue'
-  import HelloWorld from './components/HelloWorld.vue'
 
   export default {
     name: 'App',
     components: {
-      Header, HelloWorld
+      Header,
     }
   }
 </script>
